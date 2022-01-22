32972526426ca2135a875e7e174430fbdbead980
Darya Sharnevich <darya.sharnevich@gmail.com>
2022-01-22 15:10:04 +0300

    first hw

--- projects/functions/index.js
+++ projects/functions/index.js
@@ -12,7 +12,10 @@
 
  Другими словами: функция должна возвращать в неизменном виде то, что поступает ей на вход
  */
-function returnFirstArgument(value) {}
+
+function returnFirstArgument(value) {
+  return value;
+}
 
 /*
  Задание 2:
@@ -28,7 +31,27 @@ function returnFirstArgument(value) {}
  Пример:
    sumWithDefaults(10) вернет 110
  */
-function sumWithDefaults(a, b) {}
+
+function sumWithDefaults(a, b = 100, ...c) {
+  return (
+    a +
+    b +
+    c.reduce(function (a, b) {
+      return a + b;
+    }, 0)
+  );
+}
+
+// function sumWithDefaults(a, b=100, ...c) {
+//   let result = a + b;
+
+//   for (let index = 0; index < c.length; index++) {
+
+//     result += c[index];
+//   };
+
+//   return result;
+// };
 
 /*
  Задание 3:
@@ -38,7 +61,10 @@ function sumWithDefaults(a, b) {}
  Пример:
    returnFnResult(() => 'привет') вернет 'привет'
  */
-function returnFnResult(fn) {}
+
+function returnFnResult(fn) {
+  return fn();
+}
 
 /*
  Задание 4:
@@ -56,7 +82,14 @@ function returnFnResult(fn) {}
    console.log(f()); // выведет 12
    console.log(f()); // выведет 13
  */
-function returnCounter(number) {}
+
+function returnCounter(a = 0) {
+  return () => ++a;
+}
+
+const f = returnCounter();
+
+console.log(f());
 
 /*
  Задание 5 *:
@@ -67,7 +100,20 @@ function returnCounter(number) {}
  Пример:
    returnArgumentsArray(1, 2, 3) вернет [1, 2, 3]
  */
-function returnArgumentsArray() {}
+
+function returnArgumentsArray(...arg) {
+  return arg;
+}
+
+// function returnArgumentsArray1() {
+//   let ar = [];
+
+//   for (let index = 0; index < arguments.length; index++) {
+
+//     ar.push(arguments[index]);
+//   }
+//   return ar;
+// }
 
 /*
  Задание 6 *:
@@ -84,7 +130,11 @@ function returnArgumentsArray() {}
 
    console.log(newSum()) выведет 6
  */
-function bindFunction(fn, ...args) {}
+//
+
+function bindFunction(fn, ...args) {
+  return () => fn(...args);
+}
 
 export {
   returnFirstArgument,
