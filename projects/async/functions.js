5da8e6292aebf92922de70a5c7550c0522a0aa01
Darya Sharnevich <darya.sharnevich@gmail.com>
2022-02-03 20:22:19 +0300

    hw 5 1-2 tasks completed, index.js left

--- projects/async/functions.js
+++ projects/async/functions.js
@@ -8,7 +8,13 @@
  Пример:
    delayPromise(3) // вернет promise, который будет разрешен через 3 секунды
  */
-function delayPromise(seconds) {}
+function delayPromise(seconds) {
+  return new Promise((resolve) => {
+    setTimeout(() => {
+      resolve();
+    }, seconds * 1000);
+  });
+}
 
 /*
  Задание 2:
@@ -23,6 +29,21 @@ function delayPromise(seconds) {}
  Пример:
    loadAndSortTowns().then(towns => console.log(towns)) // должна вывести в консоль отсортированный массив городов
  */
-function loadAndSortTowns() {}
+function loadAndSortTowns() {
+  return new Promise((resolve) => {
+    const xhr = new XMLHttpRequest();
+
+    xhr.open(
+      'GET',
+      'https://raw.githubusercontent.com/smelukov/citiesTest/master/cities.json'
+    );
+    xhr.responseType = 'json';
+    xhr.send();
+
+    xhr.addEventListener('load', () => {
+      resolve(xhr.response.sort((a, b) => a.name.localeCompare(b.name)));
+    });
+  });
+}
 
 export { delayPromise, loadAndSortTowns };
